1b860d5bb8ee95b6cd03628a8363919e15a2fe8a
Fabien Royer <royer.tech@outlook.com>
2026-09-08 23:55:17 +0200

    Simple JWT

--- projet/projet/urls.py
+++ projet/projet/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from rest_framework import routers
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from shop.views import CategoryViewset, ProductViewset, ArticleViewset, AdminCategoryViewset, AdminCategoryViewset
 
@@ -32,5 +33,7 @@ router.register('admin/article', AdminArticleViewset, basename='admin-article')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='obtain_tokens'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='refresh_token'),
     path('api/', include(router.urls))
 ]
